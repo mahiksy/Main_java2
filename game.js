5a3c708e709c90e57443d23d77e3787f97adf1f5
mahiksy <97805518+mahiksy@users.noreply.github.com>
2023-05-11 14:59:21 -0400

    update

--- game.js
+++ game.js
@@ -1,7 +1,6 @@
 let userScore = 0;
 let pcScore = 0;
 let userChoice = "";
-let ListID = ["1","2","3"];
 let actions = ["rock","paper","scissors"];
 const options = document.querySelectorAll(".game-board-buttons");
 const computerAction = document.querySelector(".computer-action");
@@ -15,21 +14,6 @@ let getUserChoice = function(op){
     userChoice = op;
 }
 
-function game(){
-    let pc = computerGame();
-    computerAction.setAttribute('id', actions[pc]);
-    computerAction.style.display="block";
-    controlB[1].style.display = "block";
-    controlB[0].style.display = "none";
-    resultText.textContent = playRound(userChoice,pc);
-    if(pcScore == 5 || userScore == 5){
-        controlB[1].style.display = "none";
-        controlB[2].style.display = "block";
-        resultText.textContent = checkWin();  
-    }
-    scores.textContent = `player: ${userScore} -------------------------------------- ${pcScore} :Computer`
-}
-
 let computerGame = function(){
     let randomNumber = Math.floor(Math.random() * (3));
     return randomNumber;
@@ -37,8 +21,6 @@ let computerGame = function(){
 
 function playRound(useroption, computer){
     let msg;
-    console.log(userChoice);
-    console.log(actions[computer]);
     switch (true){
         case( useroption == actions[computer]):
             msg = `It's a tie!`;
@@ -71,7 +53,6 @@ function playRound(useroption, computer){
     return msg;
 }
 
-
 function checkWin(){
     if(userScore>pcScore){
         return "You Won! Congrats!";
@@ -79,6 +60,21 @@ function checkWin(){
     else{
         return "You Lost!";
     }
+}
+
+function game(){
+    let pc = computerGame();
+    computerAction.setAttribute('id', actions[pc]);
+    computerAction.style.display="block";
+    controlB[1].style.display = "block";
+    controlB[0].style.display = "none";
+    resultText.textContent = playRound(userChoice,pc);
+    if(pcScore == 5 || userScore == 5){
+        controlB[1].style.display = "none";
+        controlB[2].style.display = "block";
+        resultText.textContent = checkWin();  
+    }
+    scores.textContent = `player: ${userScore} -------------------------------------- ${pcScore} :Computer`
 }
 
 let refreshGame = function(){
@@ -92,7 +88,6 @@ let refreshGame = function(){
     resultText.textContent = "Please choose one of the actions!";
 }
 
-//adding eventListenr to options
 options.forEach((butt)=>{
     let attribute = butt.getAttribute('id');
     butt.addEventListener("click", ()=>{
@@ -100,23 +95,25 @@ options.forEach((butt)=>{
     });
 });
 
-//add event listenr to start,controlButtons[1],and refresh buttons
-controlB[0].addEventListener('click', ()=>{
-        if(userChoice != ""){
-            game();
+controlB.forEach(button=>{
+    button.addEventListener('click', ()=>{
+        if(button == controlB[0]){
+            if(userChoice != ""){
+                game();
+            }
         }
-    }   
-);
-controlB[1].addEventListener('click',(e)=>{
-    controlB[1].style.display = "none";
-    controlB[0].style.display = "block";
-    userChoice = "";
-    resultText.textContent = "Please choose one of the actions!"
-    computerAction.style.display = "none";
-});
-controlB[2].addEventListener('click',()=>{
-    refreshGame();
-}); 
+        else if( button == controlB[1]){
+            controlB[1].style.display = "none";
+            controlB[0].style.display = "block";
+            userChoice = "";
+            resultText.textContent = "Please choose one of the actions!"
+            computerAction.style.display = "none";
+        }
+        else{
+            refreshGame();
+        }
+    });
+})
 
 
 
